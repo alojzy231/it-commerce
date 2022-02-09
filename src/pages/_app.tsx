218bc8b4bff060905b353ddf4b950d245ab8eee2
alojzy231 <alojzy231@gmail.com>
2022-02-09 20:29:31 +0100

    Added a favicon and a title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 
 import GlobalStyle from '@styles/GlobalStyle';
 import theme from '@styles/theme';
@@ -9,12 +10,18 @@ import Layout from '@generic/Layout';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <GlobalStyle />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>IT-Commerce</title>
+        <link rel="icon" href="favicon.png" type="image/x-icon" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   );
 }
 
