235a98d44d46c4a35a47a1d3dd201dc8a3b504b7
alojzy231 <alojzy231@gmail.com>
2022-04-07 16:05:09 +0200

    Fixed not opening sidebar

--- src/components/generic/Layout/Navbar/HamburgerMenu/index.tsx
+++ src/components/generic/Layout/Navbar/HamburgerMenu/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import HamburgerIcon from '@icons/HamburgerIcon';
 import IconButton from '@generic/buttons/IconButton';
@@ -8,7 +9,12 @@ import Sidebar from '../Sidebar';
 import HamburgerMenuContainer from './HamburgerMenu.styles';
 
 export default function HamburgerMenu(): JSX.Element {
+  const router = useRouter();
   const [isSidebarOpened, toggleIsSidebarOpened] = useState<boolean>(false);
+
+  useEffect(() => {
+    toggleIsSidebarOpened(false);
+  }, [router]);
 
   const handleOpenSidebar = (): void => toggleIsSidebarOpened(true);
   const handleCloseSidebar = (): void => toggleIsSidebarOpened(false);
