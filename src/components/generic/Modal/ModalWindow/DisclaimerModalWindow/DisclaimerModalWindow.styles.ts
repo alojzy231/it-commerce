22b006596f1289492f5cae269ae942a501ccf308
alojzy231 <alojzy231@gmail.com>
2022-04-11 21:04:59 +0200

    Changedstyle of the disclaimer modal text

--- src/components/generic/Modal/ModalWindow/DisclaimerModalWindow/DisclaimerModalWindow.styles.ts
+++ src/components/generic/Modal/ModalWindow/DisclaimerModalWindow/DisclaimerModalWindow.styles.ts
@@ -4,13 +4,23 @@ import { Header5, Header6 } from '@styles/typography';
 import GenericButton from '@generic/buttons/GenericButton';
 
 export const DisclaimerModalWindowTitle = styled.h4`
+  margin-bottom: 3.2rem;
+
+  font-weight: bold;
+
+  text-align: center;
+
   @media (max-width: ${({ theme: { medias } }): string => medias.mobile}) {
+    margin-bottom: 1.6rem;
+
     ${Header5};
+    font-weight: bold;
   }
 `;
 
 export const DisclaimerModalWindowText = styled.h5`
   margin-bottom: 3.2rem;
+
   @media (max-width: ${({ theme: { medias } }): string => medias.mobile}) {
     ${Header6};
     margin-bottom: 1.6rem;
